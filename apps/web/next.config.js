52c0d4144555c6cfbc2b7999d0f858bc3a5d4fa3
Abhilash <chabhilashraju@gmail.com>
2026-05-31 23:11:26 -0500

    fix: hoist React to single instance via .npmrc to fix useContext null in production build

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
-
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['@skove/sdk'],
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      react: path.dirname(require.resolve('react/package.json')),
-      'react-dom': path.dirname(require.resolve('react-dom/package.json')),
-    }
-    return config
-  },
 }
 
 module.exports = nextConfig
